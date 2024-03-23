0cc4ce2e14449b3001374b091e569292885d6132
Brieuc Dubois <git@bhasher.com>
2024-03-23 14:36:40 +0100

    nvim plugins & hyprland bindings

--- root/home/bhasher/.config/nvim/init.lua
+++ root/home/bhasher/.config/nvim/init.lua
@@ -14,42 +14,127 @@ vim.opt.expandtab = false
 vim.opt.number = true
 vim.opt.relativenumber = true
 
-vim.opt.wildmode = 'longest,list'
+vim.opt.wildmode = "longest,list"
 
-vim.opt.syntax = 'on'
+vim.opt.syntax = "on"
 
 vim.opt.ttyfast = true
 vim.opt.swapfile = false
-vim.opt.mouse = 'a'
+vim.opt.mouse = "a"
 
 vim.opt.spell = true
-vim.opt.spelllang = 'en' -- ,fr'
-vim.api.nvim_set_hl(0, "SpellBad", { ctermbg=238 })
+vim.opt.spelllang = "en" -- ,fr'
+vim.api.nvim_set_hl(0, "SpellBad", { ctermbg = 238 })
+
+vim.cmd([[colorscheme desert]])
 
 -- Plugins
-local Plug = vim.fn['plug#']
 
-vim.call('plug#begin')
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 
-Plug('neoclide/coc.nvim', {['branch'] = 'release'})
-Plug('francoiscabrol/ranger.vim')
-Plug('rbgrouleff/bclose.vim')
--- Plug('zbirenbaum/copilot.lua')
-Plug('github/copilot.vim')
-Plug('nvim-treesitter/nvim-treesitter', {['do'] = ':TSUpdate'})
+if not vim.loop.fs_stat(lazypath) then
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable",
+		"--depth=1",
+		lazypath,
+	})
+end
+vim.opt.rtp:prepend(lazypath)
 
-vim.call('plug#end')
-
--- inoremap <silent><expr> <CR> coc#pum#visible() ? coc#pum#confirm()
---                            \: "\<C-g>u\<CR>\<c-r>=coc#on_enter()\<CR>"
-
-vim.g.ranger_replace_netrw = 1
-vim.keymap.set('i', '<C-Right>', '<Plug>(copilot-accept-word)')
-
-require('nvim-treesitter.configs').setup({
-	ensure_installed = { "go", "lua", "vim", "vimdoc", "sql", "json", },
-  highlight = {
-    enable = true,
-  },
+require("lazy").setup({
+	{
+		-- Auto completion
+		"neoclide/coc.nvim",
+		branch = "release",
+	},
+	{
+		-- LSP
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+	},
+	{
+		-- Copilot
+		"github/copilot.vim",
+		--config = function()
+		--	require("copilot.vim").setup({
+		--		filetypes = {
+		--			["."] = true,
+		--		}
+		--	})
+		-- end,
+	},
+	{
+		-- Ranger file explorer
+		"kelly-lin/ranger.nvim",
+		config = function()
+			require("ranger-nvim").setup({ replace_netrw = true })
+		end,
+	},
+	{
+		-- Formatter
+		"stevearc/conform.nvim",
+		opts = {},
+	},
+	{
+		-- Auto pairs
+		"altermo/ultimate-autopair.nvim",
+		event = { "InsertEnter", "CmdlineEnter" },
+		branch = "v0.6", --recommended as each new version will have breaking changes
+		opts = {},
+	},
 })
 
+require("conform").setup({
+	formatters_by_ft = {
+		lua = { "stylua" },
+		python = { "black" },
+		javascript = { "prettier" },
+		typescript = { "prettier" },
+		svelte = { "prettier" },
+		html = { "prettier" },
+		css = { "prettier" },
+		md = { "prettier" },
+		less = { "prettier" },
+		scss = { "prettier" },
+		json = { "prettier" },
+		yaml = { "prettier" },
+		toml = { "prettier" },
+		java = { "google-java-format" },
+	},
+	format_on_save = {},
+})
+
+require("nvim-treesitter.configs").setup({
+	ensure_installed = { "go", "lua", "vim", "vimdoc", "sql", "json", "javascript", "typescript", "svelte", "html", "java", "markdown" },
+	highlight = {
+		enable = true,
+	},
+	autotag = {
+		enable = true,
+	},
+})
+
+
+-- mappings
+vim.keymap.set("i", "<C-Right>", "<Plug>(copilot-accept-word)")
+vim.keymap.set(
+	"i",
+	"<CR>",
+	"coc#pum#visible() ? coc#pum#next(1): '<CR>'",
+	{ noremap = true, silent = true, expr = true }
+)
+
+-- commands
+vim.api.nvim_create_user_command("Day", function(opts)
+	vim.cmd([[colorscheme delek]])
+end, { nargs = 0 })
+
+vim.api.nvim_create_user_command("Night", function(opts)
+	vim.cmd([[colorscheme desert]])
+end, { nargs = 0 })
+
+-- Abc
